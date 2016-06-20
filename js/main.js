d0cef8044cd94eac307cd40e173a9eefc545c46e
Jonathan Alvarez G <j@jonalvarezz.com>
2016-06-20 18:20:09 -0500

    Renderiza el botón desde el HTML para hacerlo igual que en el workshop

--- js/main.js
+++ js/main.js
@@ -7,22 +7,17 @@
     "It's a rare person who wants to hear what he doesn't want to hear. – Dick Cavett",
     "A mind troubled by doubt cannot focus on the course to victory. – Arthur Golden, Memoirs of a Geisha",
     "Have you heard that it was good to gain the day? I also say it is good to fall, battles are lost in the same spirit in which they are won. – Walt Whitman ",
-    "When you can't have what you want, it's time to start wanting what you have. – Kathleen A. Sutton.",
     "The most exciting phrase to hear in science, the one that heralds new discoveries, is not 'Eureka!' I found it!) but 'That's funny ...' – Isaac Asimov ",
     "Behind the phony tinsel of Hollywood lies the real tinsel. – Oscar Levant"
   ];
   var frasesElement = document.getElementById('frases-app');
+  var nextButton = document.getElementById('frases-button');
   var fraseIndex = 0;
 
-  // Insertamos un botón luego de las frases
-  var nextButton = document.createElement('BUTTON');
-  nextButton.textContent = "Siguiente >";
-  frasesElement.parentElement.appendChild(nextButton);
-
-  // Mostramos la primer frase
+  // Ejecutamos cambiarFrase para mostrar la primer frase
   cambiarFrase();
 
-  // Agregamos escuchador del click del botón para mostrar otra frase
+  // Agregamos un escuchador del click al botón para mostrar otra frase
   nextButton.addEventListener('click', cambiarFrase)
 
   function cambiarFrase () {
